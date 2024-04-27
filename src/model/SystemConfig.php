5f222e56cd129a9e48c39fb86229642b3806540e
807735 <117384790@qq.com>
2024-04-27 13:04:19 +0800

    1

---
+++ src/model/SystemConfig.php
@@ -0,0 +1,41 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | Library for ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2024 ThinkAdmin [ thinkadmin.top ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://thinkadmin.top
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// | 免费声明 ( https://thinkadmin.top/disclaimer )
+// +----------------------------------------------------------------------
+// | gitee 仓库地址 ：https://gitee.com/zoujingli/ThinkLibrary
+// | github 仓库地址 ：https://github.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+
+declare (strict_types=1);
+
+namespace think\admin\model;
+
+use think\admin\Model;
+
+/**
+ * 系统配置模型
+ * @class SystemConfig
+ * @package think\admin\model
+ */
+class SystemConfig extends Model
+{
+    /**
+     * 创建字段
+     * @var string
+     */
+    protected $createTime = 'create_at';
+
+    /**
+     * 更新字段
+     * @var string
+     */
+    protected $updateTime = 'update_at';
+}
